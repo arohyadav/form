0f5e7e174304210caac206d99ed7a5b009ff37fe
arohyadav <arohyadav8@gmail.com>
2023-05-22 16:48:22 +0530

    eve

--- client/src/Consulting.js
+++ client/src/Consulting.js
@@ -26,7 +26,7 @@ const Consulting = () => {
     e.preventDefault();
     console.log(formValues);
     try {
-      const res = await axios.post('http://localhost:5000/api/consulting', formValues);
+      const res = await axios.post('/api/consulting', formValues);
       console.log(res.data);
     } catch (err) {
       console.error(err);
